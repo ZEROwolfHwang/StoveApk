a7456800b1dc61f460ef0eccd678b56cc38710ea
ZEROwolf Hwang <zerowolfhwang@ZEROwolfdeMacBook-Pro.local>
2019-07-25 11:54:07 +0800

    add tenvine-channel-1.0.aar

--- app/src/main/java/com/tenvine/stoveapk/MainActivity.java
+++ app/src/main/java/com/tenvine/stoveapk/MainActivity.java
@@ -4,18 +4,33 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.leon.channel.helper.ChannelReaderUtil;
+import com.tenvine.channel.helper.ChannelReaderUtil;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView mTextView;
+//    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = findViewById(R.id.channel);
+
+        TextView mTextView = findViewById(R.id.channel);
         String channel = ChannelReaderUtil.getChannel(getApplicationContext());
         mTextView.setText(channel);
+
+        TextView getAllIdValueMap = findViewById(R.id.getAllIdValueMap);
+        Map<Integer, ByteBuffer> allIdValueMap = ChannelReaderUtil.getAllIdValueMap(getApplicationContext());
+        getAllIdValueMap.setText(allIdValueMap.toString());
+
+//        TextView getApkPath = findViewById(R.id.getApkPath);
+//        String apkPath = ChannelReaderUtil.getApkPath(getApplicationContext());
+//        getApkPath.setText(apkPath);
+
+
     }
 }
